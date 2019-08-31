3374cf9e2c7a4a499a2bc7ff23d14eaff117a570
Martijn Durnez <martijn.durnez@mederi.be>
2019-08-31 20:03:22 +0200

    default patient

--- src/smartClient.js
+++ src/smartClient.js
@@ -1,13 +1,12 @@
 const getSmartClient = () =>
   new Promise((resolve, reject) => {
-    if (process.env.NODE_ENV === 'development' && false) {
+    if (process.env.NODE_ENV === 'development') {
       /* eslint-disable-next-line */
     const smart = FHIR.client({
 
         // === Cerner sandbox ===
-        // serviceUrl:
-        //   'https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca',
-        // patientId: '1316024'
+       //  serviceUrl:       'https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca',
+         patientId: '389bac6b-5b19-43c2-90a6-79afa9ba59e7',
 
         // === SMART on FHIR sandbox ===
         serviceUrl: 'https://r4.smarthealthit.org'
