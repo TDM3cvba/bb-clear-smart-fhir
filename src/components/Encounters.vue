6d3bbea92524f6c5289a1c68ad322bb382be4f8c
Martijn Durnez <martijn.durnez@mederi.be>
2019-09-04 14:56:15 +0200

    giffing

--- src/components/Encounters.vue
+++ src/components/Encounters.vue
@@ -100,7 +100,7 @@ export default {
       }
       var inss = this.fakeInss;
       var fakeProvince = "Oost-Vlaanderen";
-      var fakeCountry = "Belgium";
+      var fakeCountry = "BE";
       var fakeNihiiCareProvider = "17385467004";
       var gender = 0;
       switch(this.patient.gender)
